38f06beed82edcf59fb7b6400b6111ae36e0c68d
bitdragon50 <boseme@hotmail.co.uk>
2024-02-04 09:33:55 +0000

    Alan to options

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -30,4 +29,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples (num_of_apples: i32) -> i32 {
+    if num_of_apples <= 40 {
+        return 2 * num_of_apples
+    } else {
+        return num_of_apples
+    }
 }
